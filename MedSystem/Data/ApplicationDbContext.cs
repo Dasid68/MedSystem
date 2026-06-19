ac5972af095b09c98203f1e15981154ac94580c8
David <david.kostovski@yahoo.com>
2026-06-19 18:31:05 +0200

    Project Creation

---
+++ MedSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedSystem.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
