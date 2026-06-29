c4d0e081615424f338868ac71eff8b1dd8d24238
David <david.kostovski@yahoo.com>
2026-06-30 00:14:05 +0200

    Admin navbar skroz sreden, dodadeni kontroleri i views, za dodadenite linkovi vo navbarot

---
+++ MedSystem/Areas/Admin/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedSystem.Areas.Admin.Controllers;
+
+public class PatientController : AdminBaseController
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ MedSystem/Areas/Admin/Controllers/SpecializationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedSystem.Areas.Admin.Controllers;
+
+public class SpecializationController : AdminBaseController
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
