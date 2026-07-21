24c734b302e5cce19eec7f850bb78dfca15e0992
Dasid68 <david.kostovski@yahoo.com>
2026-07-21 23:55:25 +0200

    Dodadena moznost za detali za pacientite vo kontrolna tabla

--- MedSystem/Services/SystemLogService.cs
+++ MedSystem/Services/SystemLogService.cs
@@ -1,27 +1,29 @@
 ﻿using MedSystem.Areas.Admin.Models;
 using MedSystem.Data;
 
-namespace MedSystem.Services;
-
-public interface ISystemLogService
+namespace MedSystem.Services
 {
-    Task LogAsync(string message, LogAction action, LogType logType, string? performedBy = null);
-}
 
-public class SystemLogService(ApplicationDbContext context) : ISystemLogService
-{
-    public async Task LogAsync(string message, LogAction action, LogType logType, string? performedBy = null)
+    public interface ISystemLogService
     {
-        var log = new SystemLog
-        {
-            Message = message,
-            Action = action,
-            LogType = logType,
-            PerformedBy = performedBy,
-            Timestamp = DateTime.Now
-        };
+        Task LogAsync(string message, LogAction action, LogType logType, string? performedBy = null);
+    }
 
-        context.SystemLogs.Add(log);
-        await context.SaveChangesAsync();
+    public class SystemLogService(ApplicationDbContext context) : ISystemLogService
+    {
+        public async Task LogAsync(string message, LogAction action, LogType logType, string? performedBy = null)
+        {
+            var log = new SystemLog
+            {
+                Message = message,
+                Action = action,
+                LogType = logType,
+                PerformedBy = performedBy,
+                Timestamp = DateTime.Now
+            };
+
+            context.SystemLogs.Add(log);
+            await context.SaveChangesAsync();
+        }
     }
 }
